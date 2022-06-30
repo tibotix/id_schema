4be71fc6d600be73f5f685cef06b68705e9f5c4b
tibotix <tizian@seehaus.net>
2022-06-30 19:25:29 +0200

    [enh] fixed packages and prepare for release v1.0.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="id_schema",
-    version="1.0.0",
+    version="1.0.1",
     description="ID Schema",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -17,7 +17,7 @@ setup(
     author_email="tizian@seehaus.net",
     url="https://github.com/tibotix/id_schema",
     package_dir={"id_schema": "src"},
-    packages=["id_schema"],
+    packages=["id_schema", "id_schema.components"],
     install_requires=[],
     extras_require={"test": ["pytest"]},
     python_requires=">=3.8, <4",
