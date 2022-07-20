e7e2c5bd7e6f7c2475f345b5c9067916ecb95d49
tibotix <tizian@seehaus.net>
2022-07-20 12:52:35 +0200

    [enh] Prepared for release v1.0.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="id_schema",
-    version="1.0.3",
+    version="1.0.4",
     description="ID Schema",
     long_description=long_description,
     long_description_content_type="text/markdown",
